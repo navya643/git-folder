dd0d2f532bba32c485c8b5b7c7c302f4196f078d
navya643 <navya.telukondi643@gmail.com>
2025-02-12 09:22:51 +0530

    Add files via upload

---
+++ Demo067.java
@@ -0,0 +1,10 @@
+class Demo067{
+    public static void main(String[] args){
+        int[] arr = {2,3,4,6,8,7};
+        for(int i=0;i<arr.length;i++){
+            if(i % 2 == 0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
